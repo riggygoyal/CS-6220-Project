038cce4d53e528f193c6e2512cccf903fc8b0b40
Brandon Ding <dexon888@gmail.com>
2023-11-27 15:47:29 -0500

    Added neural network starter

---
+++ neural_network.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, BatchNormalization
+from tensorflow.keras.optimizers import Adam
+
+# Preprocessing and Sample Data here
+
+
+# Split Data
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42)
+
+# Standardize Features
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+
+# Neural Network Model Setup
+model = Sequential([
+    Dense(128, activation='relu', input_shape=(X_train_scaled.shape[1],)),
+    BatchNormalization(),
+    Dropout(0.5),
+    Dense(64, activation='relu'),
+    Dense(1)  # Output layer for regression
+])
+
+# Model Compilation
+model.compile(optimizer=Adam(learning_rate=0.001), loss='mean_squared_error')
